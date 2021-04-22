287673e39dfcc7efa5cdf6ac4e03e9ce1c626dcf
mattmiller1989 <mattmiller0989@gmail.com>
2021-04-22 15:01:54 -0500

    updated isEndTests() for new Move class

--- src/test/unittest/board/IsEndTests.java
+++ src/test/unittest/board/IsEndTests.java
@@ -25,44 +25,44 @@ public class IsEndTests extends TestCase {
 	
 	public void testNoLegalMoves() {
 		try {
-			myBoard.MakeMove(0, 10);
-			myBoard.MakeMove(1, 6);
+			myBoard.MakeMove(new Move(0, 10));
+			myBoard.MakeMove(new Move(1, 6));
 			
-			myBoard.MakeMove(0, 4);
-			myBoard.MakeMove(1, 7);
+			myBoard.MakeMove(new Move(0, 4));
+			myBoard.MakeMove(new Move(1, 7));
 			
-			myBoard.MakeMove(0, 13); 
-			myBoard.MakeMove(1, 11);
+			myBoard.MakeMove(new Move(0, 13)); 
+			myBoard.MakeMove(new Move(1, 11));
 
-			myBoard.MakeMove(0, 19);
-			myBoard.MakeMove(1, 16);
+			myBoard.MakeMove(new Move(0, 19));
+			myBoard.MakeMove(new Move(1, 16));
 
-			myBoard.MakeMove(0, 18);
-			myBoard.MakeMove(1, 17);
+			myBoard.MakeMove(new Move(0, 18));
+			myBoard.MakeMove(new Move(1, 17));
 
-			myBoard.MakeMove(0, 5);
-			myBoard.MakeMove(1, 12);
+			myBoard.MakeMove(new Move(0, 5));
+			myBoard.MakeMove(new Move(1, 12));
 
-			myBoard.MakeMove(0, 9);
-			myBoard.MakeMove(1, 15); // formed mill
-			myBoard.RemoveMan(1,  9); // remove piece
+			myBoard.MakeMove(new Move(0, 9));
+			myBoard.MakeMove(new Move(1, 15)); // formed mill
+			myBoard.RemoveMan(new Move(1,  9, true)); // remove piece
 
-			myBoard.MakeMove(0, 9);
-			myBoard.MakeMove(1, 8); // formed mill
-			myBoard.RemoveMan(1,  9); // remove piece
+			myBoard.MakeMove(new Move(0, 9));
+			myBoard.MakeMove(new Move(1, 8)); // formed mill
+			myBoard.RemoveMan(new Move(1,  9, true)); // remove piece
 
-			myBoard.MakeMove(0, 9);
-			myBoard.MakeMove(1, 0);
+			myBoard.MakeMove(new Move(0, 9));
+			myBoard.MakeMove(new Move(1, 0));
 
-			myBoard.MakeMove(0, 3, 10); // formed mill
-			myBoard.RemoveMan(0,  0); // remove piece
-			myBoard.MakeMove(1, 10, 11);
+			myBoard.MakeMove(new Move(0, 3, 10)); // formed mill
+			myBoard.RemoveMan(new Move(0,  0, true)); // remove piece
+			myBoard.MakeMove(new Move(1, 10, 11));
 			
-			myBoard.MakeMove(0, 0, 9); 
-			myBoard.MakeMove(1, 11, 10); // formed mill
-			myBoard.RemoveMan(1, 0); // remove piece
+			myBoard.MakeMove(new Move(0, 0, 9)); 
+			myBoard.MakeMove(new Move(1, 11, 10)); // formed mill
+			myBoard.RemoveMan(new Move(1, 0, true)); // remove piece
 			
-			myBoard.MakeMove(0, 10, 18); // player 2 is now boxed in with no legal moves
+			myBoard.MakeMove(new Move(0, 10, 18)); // player 2 is now boxed in with no legal moves
 		} catch (Exception e) {
 			fail();
 		}
