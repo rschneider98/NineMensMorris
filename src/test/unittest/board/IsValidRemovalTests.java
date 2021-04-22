550dbc50839223d3b9b18857a5698d699d8313f0
mattmiller1989 <mattmiller0989@gmail.com>
2021-04-22 15:57:48 -0500

    updated IsValidRemoval() to reflect refactoring

--- src/test/unittest/board/IsValidRemovalTests.java
+++ src/test/unittest/board/IsValidRemovalTests.java
@@ -84,5 +84,81 @@ public class IsValidRemovalTests extends TestCase {
 		assertTrue(myBoard.everyPieceAMill(0));
 		assertFalse(myBoard.everyPieceAMill(1));
 	}
+	public void testValidRemovalNewMove() {
+		try {
+			myBoard.MakeMove(new Move(0, 0));
+			myBoard.MakeMove(new Move(1, 9));
+			myBoard.MakeMove(new Move(0, 1));
+			myBoard.MakeMove(new Move(1, 20));
+			myBoard.MakeMove(new Move(0, 2));
+		} catch (Exception e) {
+			fail();
+		}
+		Move currentRemoval=new Move(0, 9, true);
+		assertTrue(myBoard.IsValidRemoval(currentRemoval.getPlayerTurn(),currentRemoval.getLocationTo()));
+	}
+	
+	
+	public void testInvalidTurnNewMove() {
+		try {
+			myBoard.MakeMove(new Move(0, 0));
+			myBoard.MakeMove(new Move(1, 9));
+			myBoard.MakeMove(new Move(0, 1));
+			myBoard.MakeMove(new Move(1, 20));
+			myBoard.MakeMove(new Move(0, 2));
+		} catch (Exception e) {
+			fail();
+		}
+		Move currentRemoval=new Move(1, 9, true);
+		assertFalse(myBoard.IsValidRemoval(currentRemoval.getPlayerTurn(),currentRemoval.getLocationTo()));
+	}
+	
+	
+	public void testEmptyLocationNewMove() {
+		try {
+			myBoard.MakeMove(new Move(0, 0));
+			myBoard.MakeMove(new Move(1, 9));
+			myBoard.MakeMove(new Move(0, 1));
+			myBoard.MakeMove(new Move(1, 20));
+			myBoard.MakeMove(new Move(0, 2));
+		} catch (Exception e) {
+			fail();
+		}
+		Move currentRemoval=new Move(0, 10, true);
+		assertFalse(myBoard.IsValidRemoval(currentRemoval.getPlayerTurn(),currentRemoval.getLocationTo()));
+	}
+	
+	
+	public void testOwnPieceNewMove() {
+		try {
+			myBoard.MakeMove(new Move(0, 0));
+			myBoard.MakeMove(new Move(1, 9));
+			myBoard.MakeMove(new Move(0, 1));
+			myBoard.MakeMove(new Move(1, 20));
+			myBoard.MakeMove(new Move(0, 2));
+		} catch (Exception e) {
+			fail();
+		}
+		Move currentRemoval=new Move(0, 0, true);
+		assertFalse(myBoard.IsValidRemoval(currentRemoval.getPlayerTurn(),currentRemoval.getLocationTo()));
+	}
+	
+	public void testEveryPieceAMillNewMove() {
+		try {
+			myBoard.MakeMove(new Move(0, 0));
+			myBoard.MakeMove(new Move(1, 3));
+			myBoard.MakeMove(new Move(0, 1));
+			myBoard.MakeMove(new Move(1, 10));
+			myBoard.MakeMove(new Move(0, 2));
+			
+			
+			
+		} catch (Exception e) {
+			
+			fail();
+		}
+		assertTrue(myBoard.everyPieceAMill(0));
+		assertFalse(myBoard.everyPieceAMill(1));
+	}
 }
 
